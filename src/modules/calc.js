c934c0208ac191e3cc0ba3770e5cb104c375d68f
Sergey <isb9797@gmail.com>
2020-05-08 19:24:32 +0300

    Lesson 28 complete. Fix anamateTotal()

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,57 +1,70 @@
 const calc = (price = 100) => {
-  const calcBlock = document.querySelector(".calc-block"),
-      calcType = document.querySelector(".calc-type"),
-      calcSquare = document.querySelector(".calc-square"),
-      calcDay = document.querySelector(".calc-day"),
-      calcCount = document.querySelector(".calc-count"),
-      totalValue = document.getElementById("total");
+const calcBlock = document.querySelector(".calc-block"),
+    calcType = document.querySelector(".calc-type"),
+    calcSquare = document.querySelector(".calc-square"),
+    calcDay = document.querySelector(".calc-day"),
+    calcCount = document.querySelector(".calc-count"),
+    totalValue = document.getElementById("total");
 
-  const countSum = () => {
-      let total = 0,
-          countAnimate = 0,
-          countValue = 1,
-          dayValue = 1;
-      const typeValue = +calcType.options[calcType.selectedIndex].value,
-          squareValue = +calcSquare.value;
+    const countSum = () => {
+    let total = 0,
+        countAnimate = 0,
+        countValue = 1,
+        dayValue = 1;
+    const typeValue = +calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
 
-      if (squareValue === "" && calcDay === "" && calcCount === "") {
-          totalValue.textContent = 0;
-      }
+    if (squareValue === "" && calcDay === "" && calcCount === "") {
+        totalValue.textContent = 0;
+    }
 
-      if (calcCount.value > 1) {
-          countValue += (calcCount.value - 1) / 10;
-      }
+    if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+    }
 
-      if (calcDay.value && calcDay.value < 5) {
-          dayValue *= 2;
-      } else if (calcDay.value && calcDay.value < 10) {
-          dayValue *= 1.5;
-      }
+    if (calcDay.value && calcDay.value < 5) {
+      dayValue *= 2;
+    } else if (calcDay.value && calcDay.value < 10) {
+      dayValue *= 1.5;
+    }
 
-      //Анимация для калькулятора
-      const animateTotal = () => {
-          countAnimate += 10;
-          totalValue.textContent = countAnimate;
+    //Анимация для калькулятора
+    const animateTotal = () => {
+        let start = Date.now(); //? Начало анимации
 
-          if (countAnimate < total) {
-              requestAnimationFrame(animateTotal);
-          }
-      };
+        let timer = setInterval(function() {
+        let timePassed = Date.now() - start;
 
-      if (typeValue && squareValue) {
-          total = price * typeValue * squareValue * countValue * dayValue;
-          requestAnimationFrame(animateTotal);
-      }
+        if (timePassed >= 2000) {
+            clearInterval(timer); //? закончить анимацию через 2 секунды
+            return;
+        }
 
-      totalValue.textContent = 0;
-  };
-  calcBlock.addEventListener("change", event => {
-      const target = event.target;
+        // отрисовать анимацию на момент timePassed, прошедший с начала анимации
+        draw(timePassed);
 
-      if (target.matches("select") || target.matches("input")) {
-          countSum();
-      }
-  });
+        }, 20);
+
+        function draw(timePassed) {
+            totalValue.textContent = timePassed / 5 + 'px';
+        }
+    };
+
+    if (typeValue && squareValue) {
+      total = price * typeValue * squareValue * countValue * dayValue;
+        animateTotal();
+        totalValue.textContent = total;
+    }
+
+    totalValue.textContent = 0;
+    };
+    calcBlock.addEventListener("change", (event) => {
+    const target = event.target;
+
+    if (target.matches("select") || target.matches("input")) {
+        countSum();
+    }
+    });
 };
 
 export default calc;
